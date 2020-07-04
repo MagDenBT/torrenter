b8f040b660a26162f15f2f73de0831dc0d488045
MagDenBT <44881346+MagDenBT@users.noreply.github.com>
2020-07-05 02:17:16 +0300

    Building 05/07/2020

---
+++ Torrneter/IDTorrent.cs
@@ -0,0 +1,15 @@
+﻿namespace Torrneter
+{
+    public class IDTorrent
+    {
+        private string[] IDs;
+        private bool RecentlyActive;
+
+        IDTorrent(params string[] ID)
+        {
+            IDs = ID;
+        }
+
+        public IDTorrent(bool RecentLy) { }
+    }
+}
--- Torrneter/IQueryFactory.cs
+++ Torrneter/IQueryFactory.cs
@@ -24,14 +24,32 @@ namespace Torrneter
         /// Может остановить, запустить, проверить или реаноснировать торрент
         /// </summary>
         /// <param name="action">Что нужно сделать</param>
-        /// <param name="ID">Список ИД торрентов, пустой список - значит ВСЕ торренты</param>
-        void TorrentAction(TorrentActions action, List<int> ID);
+        /// <param name="ID">Массив ИД торрентов, пустой массив - значит ВСЕ торренты</param>
+        void TorrentAction(TorrentActions action, params int[] ID);
 
         #endregion
 
         #region Torrent Mutators
 
         void TorrentSet(TorrentMutators mutators);
+        #endregion
+
+        #region Torrent Accessors
+        TorrentInfo TorrentGet(params int[] IDs);
+        TorrentInfo TorrentGetRecently();
+        #endregion
+
+        #region Adding a Torrent
+        TorrentAdded TorrentAdd(NewTorrent NewTorrent);
+        #endregion
+
+        #region Removing a Torrent
+        void TorrentRemove(IDTorrent ID, bool DeletelocalData);
+        void TorrentRemoveRecently(bool DeletelocalData)
+        #endregion
+
+        #region Moving a Torrent
+          void TorrentSetLocation()
         #endregion
     }
 }
